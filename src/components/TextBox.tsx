924147cde618b14a130a72a52d14df69815e38d9
Vimal Pandey <vimal@underpinservices.com>
2025-03-25 11:23:34 +0530

    text box modified

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -6,10 +6,11 @@ interface TextBoxProps {
 }
 
 const TextBox = ({ title, description }: TextBoxProps) => {
+  //max-w-[90vw] md:max-w-[70vw]
   return (
-    <section className="max-w-[90vw] md:max-w-[70vw] mx-auto flex flex-col md:flex-row justify-between space-x-32 mt-6 md:mt-0">
-      <h1 className="text-4xl md:text-5xl font-medium uppercase">{title}</h1>
-      <p className="text-xl md:text-2xl font-normal max-w-xl">{description}</p>
+    <section className="max-w-[90vw] md:max-w-[70vw] mx-auto flex flex-col md:flex-row justify-between  space-x-32 mt-6 md:mt-0 py-4 sm:py-0 ">
+      <h1 className="text-xl sm:text-xl md:text-2xl lg:text-4xl font-medium uppercase py-1 sm:py-0">{title}</h1>
+      <p className="text-md sm:text-md md:text-lg lg:text-2xl font-normal max-w-xl font-sans">{description}</p>
     </section>
   );
 };
