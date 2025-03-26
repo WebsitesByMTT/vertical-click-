57225d8e366783d0284b78682ab1a97cbe65732c
Saurabh Raturi <saurabh@underpinservices.com>
2025-03-26 09:50:32 +0530

    feat: Add Google Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         className={`${bandeins.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-EK11VJ6YW4" />
       </body>
     </html>
   );
